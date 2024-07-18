d563eb4678fb30a995d61bb58c3283675ac74cf1
Gomnonix <lee20481997@gmail.com>
2024-07-18 15:01:17 +0900

    dosirock CI/CD

--- dosirock/urls.py
+++ dosirock/urls.py
@@ -14,16 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
+from django.http import HttpRequest, HttpResponse
 from django.urls import path
-from django.http import HttpResponse
 
 
-def hello_test(request):
-    return HttpResponse('Hello World')
+def hello_test(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Hello World")
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', hello_test),
+    path("admin/", admin.site.urls),
+    path("hello/", hello_test),
 ]
