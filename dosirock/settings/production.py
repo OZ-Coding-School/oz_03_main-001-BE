f84eb7b30419923c51ba803241796f3144402995
rbwo552 <rbwo552@naver.com>
2024-07-22 16:21:44 +0900

    local 설정, production 설정 분리

---
+++ dosirock/settings/production.py
@@ -0,0 +1,15 @@
+from .base import *
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.getenv("DB_HOST"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASS"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
