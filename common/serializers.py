a8d9e5f74b7b2b7dc82e29c35938be85dcc4b1c5
rbwo552 <rbwo552@naver.com>
2024-07-23 17:57:41 +0900

    알레르기 모델 수정 및 serializer 생성

---
+++ common/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Allergy
+
+
+class AllergySerializer(serializers.ModelSerializer[Allergy]):
+    class Meta:
+        model = Allergy
+        fields = "__all__"
