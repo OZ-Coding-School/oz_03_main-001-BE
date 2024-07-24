9f5eef6b0890d33d6fa3883deaced9754a6ddb72
rbwo552 <rbwo552@naver.com>
2024-07-24 15:38:34 +0900

    알레르기 모델 수정 및 모델 어드민 생성

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Allergy
+
+
+@admin.register(Allergy)
+class AllergyAdmin(admin.ModelAdmin):  # type: ignore
+    pass
